e472496b3b90f4f831d346e4432b184f66ba601c
Lene Julien <julien.lene@gmail.com>
2019-10-07 12:04:09 +0200

    [DRIVE] change config database

--- drive/config.py
+++ drive/config.py
@@ -6,9 +6,8 @@ class Config:
     DEBUG = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:290442@localhost:5432/test_kf_backend'
-   # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:290442@localhost:5432/test_kf_backend'
-
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:290442@localhost:5432/test_db'
+    # A CHANGER POUR L"INTEGRATION DANS LA VM!!!
 class ProductionConfig(Config):
     DEBUG = False
 
